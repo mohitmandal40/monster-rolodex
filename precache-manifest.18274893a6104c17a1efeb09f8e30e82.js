7317674d16d378a5e0a983ff9316840b82b927dc
Mohit Mandal <mohitmandal40@gmail.com>
2020-09-14 21:11:54 +0530

    Updates

---
+++ precache-manifest.18274893a6104c17a1efeb09f8e30e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8dcc867b4dac31bd647a593848b84be",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5e2a3a3c354a7cea52a2",
+    "url": "/monster-rolodex/static/css/main.43ad3db3.chunk.css"
+  },
+  {
+    "revision": "1412b1c9cdcbfabcab3b",
+    "url": "/monster-rolodex/static/js/2.229c4cc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.229c4cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2a3a3c354a7cea52a2",
+    "url": "/monster-rolodex/static/js/main.138910fc.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
